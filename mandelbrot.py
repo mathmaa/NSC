7e682683ff8350a31708c59b4d4770da224ece5e
mathmaa <mathmaa@gmail.com>
2026-02-19 11:52:12 +0100

    implement compute_mandelbrot_set function

--- mandelbrot.py
+++ mandelbrot.py
@@ -17,4 +17,17 @@ print(mandelbrot_point(complex(0, 0)))
 print(mandelbrot_point(complex(1, 1)))
 print(mandelbrot_point(complex(-2, 3)))  
 
+def compute_mandelbrot_set(xmin=-2.0, xmax=1.0, ymin=-1.5, ymax=1.5, width=100, height=100, max_iter=100):
+	xvalues = np.linspace(xmin, xmax, width)
+	yvalues = np.linspace(ymin, ymax, height)
 
+	result = np.zeros((height, width), dtype=int)
+	for i in range(height):
+		for j in range(width):
+			c = complex(xvalues[j], yvalues[i])
+			result[i, j] = mandelbrot_point(c, max_iter)
+
+	return result
+
+grid = compute_mandelbrot_set(100, 100, max_iter=100)
+print(grid.shape)  # Should be (100, 100)
