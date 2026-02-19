2992c7ec2350f5a7f4704f4bb2d6a2f6623aa14d
mathmaa <mathmaa@gmail.com>
2026-02-19 12:03:26 +0100

    saving mandelbrot as png and added gitignore to ignore the png

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,6 +4,7 @@ Author : Mathias
 Course : Numerical Scientific computing
 """
 import numpy as np
+import matplotlib.pyplot as plt
 import time
 
 def mandelbrot_point(c, max_iter=100):
@@ -35,3 +36,8 @@ result = compute_mandelbrot_set(xmin=-2.0, xmax=1.0,
 								width=1024, height=1024)
 time_taken = time.time() - start
 print(f"Time taken to compute Mandelbrot set: {time_taken:.2f} seconds")
+
+plt.imshow(result, extent=(-2, 1, -1.5, 1.5), cmap='hot')
+plt.colorbar()
+plt.title('Mandelbrot Set')
+plt.savefig('mandelbrot.png')
